1382db021c17fa259e6c6a3eddd36f24fe8a0279
huayu <huayu@DESKTOP-N5K2GI1>
2020-11-27 00:40:50 +0800

    aaaa

---
+++ src/com/liuxing/TestOther.java
@@ -0,0 +1,10 @@
+package com.liuxing;
+
+import org.apache.log4j.Logger;
+
+public class TestOther {
+	private static Logger log = Logger.getLogger(TestOther.class);
+	static {
+		log.info("英雄不问出处");
+	}
+}
